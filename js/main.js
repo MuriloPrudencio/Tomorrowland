92b8e8ef4c8d6b59175e32d3a38bece6ccb42c32
MuriloPrudencio <90528471+MuriloPrudencio@users.noreply.github.com>
2021-10-18 01:05:17 -0300

    Tomorrowland

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
